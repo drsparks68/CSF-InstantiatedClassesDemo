c8ff3514760c9cea6bea852567d0834fc168621c
Derek Sparks <drsparks@gmail.com>
2020-04-24 11:29:48 -0700

    Create PersonModel.cs

---
+++ ConsoleUI/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class PersonModel
+    {
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+    }
+}
